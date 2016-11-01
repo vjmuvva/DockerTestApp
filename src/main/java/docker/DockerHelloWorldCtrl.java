ef5fde55e59af3dcf4bff3b8f228c43f32e50d9b
Vijay Muvva <chinna580@gmail.com>
2016-11-01 12:29:04 +0530

    First commit

---
+++ src/main/java/docker/DockerHelloWorldCtrl.java
@@ -0,0 +1,16 @@
+package docker;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by vijaymuvva on 1/11/16.
+ */
+@RestController
+public class DockerHelloWorldCtrl {
+
+    @RequestMapping("/sayHelloDocker")
+    public String sayHelloDocker() {
+        return "Hello, Happy Dockerizing :-)";
+    }
+}
---
+++ src/main/java/docker/Main.java
@@ -0,0 +1,15 @@
+package docker;
+
+/**
+ * Created by vijaymuvva on 1/11/16.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
